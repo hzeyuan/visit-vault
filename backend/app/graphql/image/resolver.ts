678909b6084f02b59b3b2dbdd24309c67c416c6b
hzy <599012428@qq.com>
2021-01-14 15:46:53 +0800

    h

--- backend/app/graphql/image/resolver.ts
+++ backend/app/graphql/image/resolver.ts
@@ -31,29 +31,6 @@ export = {
         numPages: 1,
         items: imgs as any,
       };
-      // return {
-      //   numItems: 1,
-      //   numPages: 0,
-      //   items: [
-      //     {
-      //       _id: '1',
-      //       addedOn: 'addedOn',
-      //       favorite: true,
-      //       customFields: { _id: 'customFields' },
-      //       name: '图片',
-      //       path: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa0.att.hudong.com%2F52%2F62%2F31300542679117141195629117826.jpg&refer=http%3A%2F%2Fa0.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611576383&t=63182d5eb71da0dc476244c3a9ceb76e',
-      //       meta: {
-      //         dimensions: {
-      //           height: '300',
-      //           width: '300',
-      //         },
-      //       },
-      //       actors: [actor],
-      //       labels: [label],
-      //     },
-
-      //   ],
-      // };
     },
   },
   Mutation: {
@@ -83,11 +60,12 @@ export = {
         const actorInDb = await ctx.service.actor.getById(actor);
         if (!actorInDb) throw new Error(`Actor ${actor} not found`);
       }
-
+      // 添加标签
       for (const label of args.labels || []) {
         const labelInDb = await ctx.service.label.getById(label);
         if (!labelInDb) throw new Error(`Label ${label} not found`);
       }
+      // 景点
       if (args.scene) {
         const sceneInDb = await ctx.service.scene.getById(args.scene);
         if (!sceneInDb) throw new Error(`Scene ${args.scene} not found`);
